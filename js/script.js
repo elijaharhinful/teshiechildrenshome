5b4d1b2196f353af02884fd016cb6f0aaeb9a797
Elijah Arhinful <elijaharhinful8@gmail.com>
2024-11-13 18:12:17 +0000

    feat: extra sections completed minus stats section

--- js/script.js
+++ js/script.js
@@ -1,5 +1,21 @@
-$('.sub-menu ul').hide();
+$(".sub-menu ul").hide();
 $(".sub-menu a").click(function () {
-	$(this).parent(".sub-menu").children("ul").slideToggle("100");
-	$(this).find(".right").toggleClass("fa-caret-up fa-caret-down");
+  $(this).parent(".sub-menu").children("ul").slideToggle("100");
+  $(this).find(".right").toggleClass("fa-caret-up fa-caret-down");
+});
+
+$(document).ready(function () {
+  $(".carousel").slick({
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: true,
+    adaptiveHeight: true,
+    autoplay: true,
+    autoplaySpeed: 2500,
+    prevArrow: '<button type="button" class="slick-prev"> </button>',
+    nextArrow: '<button type="button" class="slick-next"> </button>',
+  });
 });
